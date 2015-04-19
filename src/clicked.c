05bed1b11dc90360f40f76f1018c44636ce02455
philip-scott <felescoto95@hotmail.com>
2015-04-19 16:53:07 -0500

    matrix fix

--- src/clicked.c
+++ src/clicked.c
@@ -13,34 +13,34 @@ int click (int j, int k) {
 	0 0 0 0 0 0 0 0	0 * 0 0
 	matrix + 1 + y * 2
 	*/
-	if(*(matrix + j + x * k) == 0) //Cambiar el color del cuadro en el que se dio click
-		*(matrix + j + x * k) = 1;
-	else
+	if(*(matrix + j + x * k) == 1) //Cambiar el color del cuadro en el que se dio click
 		*(matrix + j + x * k) = 0;
+	else
+		*(matrix + j + x * k) = 1;
 	
 	if((k-1) >= 0){ //Cambiar cuadro de arriba
-		if(*(matrix + j + x * (k - 1)) == 0)
-			*(matrix + j + x * (k - 1)) = 1;
+		if(*(matrix + j + x * (k - 1)) == 1)
+			*(matrix + j + x * (k - 1)) = 0;
 		else
-			*(matrix + j + x * (k - 1)) = 0; 
+			*(matrix + j + x * (k - 1)) = 1; 
 	}
-	if((k+1) < y){ //Cambiar cuadro de abajo
-	 	if(*(matrix + j + x * (k + 1)) == 0)
-			*(matrix + j + x * (k + 1)) = 1;
+	if((k+1) < x){ //Cambiar cuadro de abajo
+	 	if(*(matrix + j + x * (k + 1)) == 1)
+			*(matrix + j + x * (k + 1)) = 0;
 		else
-			*(matrix + j + x * (k + 1)) = 0; 
+			*(matrix + j + x * (k + 1)) = 1; 
 	}
 	if((j-1) >= 0){ //Cambiar cuadro de la izq
-		if( *(matrix + (j - 1)+ x * k) == 0)
-			 *(matrix + (j - 1)+ x * k) = 1;
-		else
+		if( *(matrix + (j - 1)+ x * k) == 1)
 			 *(matrix + (j - 1)+ x * k) = 0;
-	}
-	if((j+1) < x){ //Cambiar cuadro de la der
-		if( *(matrix + (j + 1)+ x * k) == 0)
-			 *(matrix + (j + 1)+ x * k) = 1;
 		else
+			 *(matrix + (j - 1)+ x * k) = 1;
+	}
+	if((j+1) < y){ //Cambiar cuadro de la der
+		if( *(matrix + (j + 1)+ x * k) == 1)
 			 *(matrix + (j + 1)+ x * k) = 0;
+		else
+			 *(matrix + (j + 1)+ x * k) = 1;
 	}	
 	
 	int a,b,c;
